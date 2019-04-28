78fbf08c70826002db7bfffda6c306972750c30a
Meghna <megmath234@gmail.com>
2019-04-28 17:33:38 -0500

    Updated code

--- backend/app/controller/customer.controller.js
+++ backend/app/controller/customer.controller.js
@@ -1,9 +1,15 @@
 var mysql = require('mysql');
 const db = require('../config/db.config.js');
+//const session = require('express-session');
 var bcrypt = require('bcrypt');
+/*
+app.use(passport.initialize());
+app.use(passport.session());
+*/
 const saltRounds = 10;
 const Customer = db.customers;
 
+const jwt = require('jsonwebtoken')
 
 exports.login = (req, res) => {
     let user = req.body;
@@ -16,21 +22,29 @@ exports.login = (req, res) => {
       type: db.sequelize.QueryTypes.SELECT 
     })
     .then((rows) => {
-        //console.log("hash password from db", rows[0]['pwd']);
-        bcrypt.compare(password, rows[0]['pwd'], function(err, response) {
-          // res == true
-          if(response) {
-            userId = rows[0]['id'];
-            console.log("password matched with user id: ", userId);
-            res.json(userId);
-          }
-          else{
-            console.log("wrong password");
-          }
-      });
+      // if(rows.length>0) {
+      //   //console.log("hash password from db", rows[0]['pwd']);
+      //   bcrypt.compare(password, rows[0]['pwd'], function(err, response) {
+      //     // res == true
+      //     if(response) {
+      //       userId = rows[0]['id'];
+      //       console.log("password matched with user id: ", userId);
+      //       const token = jwt.sign({ _id: 'abc123'}, 'ujvfuincyfugugv')
+      //       res.json(token);
+      //     }
+      //     else{
+      //       console.log("wrong password");
+      //     }
+      // });
+      // }
+
+      
+            const token = jwt.sign({ _id: 'abc123'}, 'ujvfuincyfugugv')
+            res.json(token);
     });
     console.log('user', user);
 };
+
 
 cryptPassword = (password) => {
   console.log("password", password);
@@ -38,4 +52,5 @@ cryptPassword = (password) => {
   console.log("hashPass", hashPass); 
   return hashPass;
 };
- 
+
+
